f09bc64716165a9c04e0c934b361fac02b598121
Ricardo Aravena <raravena80@yahoo.com>
2017-04-29 02:09:12 +0000

    Add alternative solution for logparser

--- logp/README.md
+++ logp/README.md
@@ -24,3 +24,4 @@ Capture all the 'event x' lines only
 Solutions are in Go:
 
 - logp.go - Solution in go. Run with: go run logp.go
+- logpalt.go - Alternative solution in go using for (like while). Run with: go run logpalt.go
---
+++ logp/logpalt.go
@@ -0,0 +1,50 @@
+package main
+/*
+This is an altertive solution using for like while loops in
+other languages
+*/
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	var (
+		events    []string
+		sampleLog []string
+		l         string
+		match     bool
+	)
+
+	file, e := ioutil.ReadFile("./samplelog")
+	if e != nil {
+		fmt.Printf("File error: %v\n", e)
+		os.Exit(1)
+	}
+	sampleLog = strings.Split(string(file), "\n")
+	for len(sampleLog) > 0 {
+		l, sampleLog = sampleLog[0], sampleLog[1:]
+		nonEvent, _ := regexp.MatchString("Non event", l)
+		if nonEvent {
+			continue
+		}
+		check, _ := regexp.MatchString("Begin ", l)
+		if check {
+			l, sampleLog = sampleLog[0], sampleLog[1:]
+			match, _ = regexp.MatchString("End", l)
+			for !match {
+				events = append(events, l)
+				l, sampleLog = sampleLog[0], sampleLog[1:]
+				match, _ = regexp.MatchString("End", l)
+				//fmt.Println(l)
+			}
+
+		}
+	}
+	for _, e := range events {
+		fmt.Println(e)
+	}
+}
