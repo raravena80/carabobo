815590b60713e88ce493d531975ecd2103c94068
Ricardo Aravena <raravena80@yahoo.com>
2017-05-01 03:07:35 +0000

    Add new delchars solution

---
+++ delchars/README.md
@@ -0,0 +1,6 @@
+# delchars
+
+The problem is basically deleting all duplicate characters in an array. 
+Brute force approach.
+
+- delchars.go - Solution in go. Run with: go run delchars.go
---
+++ delchars/delchars.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var (
+	tail int
+	n int
+	arr []string
+)
+
+func main() {
+	arr = []string{"a", "a", "a", "c", "d", "c", "d", "e", "h", "j"}
+	n = len(arr)
+	if arr == nil {
+		os.Exit(0)
+	}
+	if n < 2 {
+		fmt.Println(arr)
+		os.Exit(0)
+	}
+	tail = 1
+	for i := 1; i < n; i++ {
+		for j := 0; j <= tail; j++ {
+			if arr[i] == arr[j] {
+				break
+			}
+			if j == tail {
+				arr[tail] = arr[i]
+				tail++
+				break
+			}
+
+		}
+
+	}
+	newarr := arr[0:tail]
+	fmt.Println(newarr)
+
+}
