98bcf98ac703315b9bda6ea1b6c9dd17a3cc7b27
Ricardo Aravena <raravena80@yahoo.com>
2017-05-04 04:02:12 +0000

    Add dinosaurs solution

---
+++ dinosaurs/README.md
@@ -0,0 +1,24 @@
+# dinosaurs -- Dinosaurs
+
+Problem is, given two files like this:
+
+csv file 1 (Name, length, stance)
+---------
+Procompsognathus,1.03,bipedal
+Triceratops,1.04,quadrupedal
+Velociraptor,1.20,bipedal
+Brontosaurus,3.0,quadrupedal
+csv file 2 (Name, stride, food)
+----------
+Procompsognathus,2.3,carnivore
+Velociraptor,3.21,carnivore
+Brontosaurus,0.3,herbivore
+Triceratops,1.40,herbivore
+
+and speed is: stride * length (for simplicity purposes)
+
+Output only the bipedal dinosaurs sorted by speed
+
+Solutions are in Go:
+
+- dinosaurs.go - Solution in Go. Run with: go run dinosaurs.go
---
+++ dinosaurs/dinosaurs.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var (
+		dinosaurs map[string]map[string]string
+	)
+
+	dinosaurs = make(map[string]map[string]string)
+	// Process file1 and add to data structure
+	file1, e := ioutil.ReadFile("./dinosaurs1.csv")
+	if e != nil {
+		fmt.Printf("File error: %v\n", e)
+		os.Exit(1)
+	}
+	dino1 := strings.Split(strings.TrimSpace(string(file1)), "\n")
+	for _, l := range dino1 {
+		i := strings.Split(l, ",")
+		dinosaurs[i[0]] = make(map[string]string)
+		dinosaurs[i[0]]["length"] = i[1]
+		dinosaurs[i[0]]["stance"] = i[2]
+	}
+	// Process file2 and add to data structure
+	file2, e := ioutil.ReadFile("./dinosaurs2.csv")
+	if e != nil {
+		fmt.Printf("File error: %v\n", e)
+		os.Exit(1)
+	}
+	dino2 := strings.Split(strings.TrimSpace(string(file2)), "\n")
+	for _, l := range dino2 {
+		i := strings.Split(l, ",")
+		dinosaurs[i[0]]["stride"] = i[1]
+		dinosaurs[i[0]]["food"] = i[2]
+	}
+	for k, _ := range dinosaurs {
+		stride, _ := strconv.ParseFloat(dinosaurs[k]["stride"], 64)
+		length, _ := strconv.ParseFloat(dinosaurs[k]["length"], 64)
+		dinosaurs[k]["speed"] = strconv.FormatFloat(stride*length, 'E', -1, 64)
+	}
+	// Create sorted slice [[Name, Speed], [Name, Speed]]
+	sorted := [][]string{}
+	for k, _ := range dinosaurs {
+		if dinosaurs[k]["stance"] == "bipedal" {
+			sorted = append(sorted, []string{k, dinosaurs[k]["speed"]})
+		}
+	}
+	sort.Slice(sorted, func(i, j int) bool {
+		a, _ := strconv.ParseFloat(sorted[j][1], 64)
+		b, _ := strconv.ParseFloat(sorted[i][1], 64)
+		return a < b
+	})
+	// Print the sorted array
+	for _, v := range sorted {
+		f, _ := strconv.ParseFloat(v[1], 64)
+		fmt.Printf("Name: %v, Speed: %0.2f\n", v[0], f)
+	}
+}
